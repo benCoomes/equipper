ba85d67a3fdd5601b1a63fdd691d90064aa8f61b
Ben Coomes <bcoomes96@gmail.com>
2021-07-18 18:33:23 -0400

    Add web app and controller to receive auth redirects.

---
+++ Web/Controllers/TokenExchangeController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenExchangeController : ControllerBase
+    {
+        private readonly ILogger<TokenExchangeController> _logger;
+
+        public TokenExchangeController(ILogger<TokenExchangeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get(string code, string scope, string error)
+        {
+            if(!string.IsNullOrWhiteSpace(error))
+                return $"Recieved error {error}.";
+            else
+                return $"Recieved authorization code {code} with scope {scope}.";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return $"Post was called.";
+        }
+    }
+}
