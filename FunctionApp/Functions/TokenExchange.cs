a2af83dff39305d4ab16d377bd0eceb993633de6
Ben Coomes <bcoomes96@gmail.com>
2021-12-04 11:03:01 -0500

    Add error handling to token exchange.

--- FunctionApp/Functions/TokenExchange.cs
+++ FunctionApp/Functions/TokenExchange.cs
@@ -23,19 +23,21 @@ namespace Coomes.Equipper.FunctionApp
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
-            var correlationID = Guid.NewGuid();
-            log.LogInformation("{function} {status} {cid}", "TokenExchange", "Starting", correlationID.ToString());
+            return await ErrorHandler.RunWithErrorHandling(log, async () => {
+                var correlationID = Guid.NewGuid();
+                log.LogInformation("{function} {status} {cid}", "TokenExchange", "Starting", correlationID.ToString());
 
-            string code = req.Query["_code"]; // see proxies.json and https://github.com/Azure/static-web-apps/issues/165
-            string scopeString = req.Query["scope"];
-            string error = req.Query["error"];
+                string code = req.Query["_code"]; // see proxies.json and https://github.com/Azure/static-web-apps/issues/165
+                string scopeString = req.Query["scope"];
+                string error = req.Query["error"];
 
-            log.LogInformation("Received auth code with scope '{scope}'", scopeString);
+                log.LogInformation("Received auth code with scope '{scope}'", scopeString);
 
-            var token = await ExecuteTokenExchange(code, scopeString, error, log);
-            
-            log.LogInformation("{function} {status} {cid}", "TokenExchange", "Success", correlationID.ToString());
-            return new OkResult();
+                var token = await ExecuteTokenExchange(code, scopeString, error, log);
+                
+                log.LogInformation("{function} {status} {cid}", "TokenExchange", "Success", correlationID.ToString());
+                return new OkResult();
+            });
         }
 
         private static async Task<string> ExecuteTokenExchange(string code, string scopesString, string error, ILogger logger) 
